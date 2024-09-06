877f8fb9da621513815c64c9c21f190de0388964
rcuriel4 <rcuriel4@mail.pima.edu>
2024-09-05 20:18:27 -0700

    lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+# Author: Anthony Curiel
+#First Python lab in CIS129
+#Removed the Hello World print
+#Now prints a short story
+print('A young boy finds a hidden door in his grandparents basement.')
+print('As he opens the door he discovers a room filled with portraits.')
+print('Each portrait a moving image of their family and their futures.')
