1d19159f2a47ddafb1832e7382492be6511a9f70
rcuriel4 <rcuriel4@mail.pima.edu>
2024-09-30 20:24:59 -0700

    Update # Lab 5 The Bottle Return Program.py

--- # Lab 5 The Bottle Return Program.py
+++ # Lab 5 The Bottle Return Program.py
@@ -4,7 +4,6 @@
 #Lab 5 The Bottle Return Program
 
 # Step 1: Declare variables below 
-totalBottles = 0
 todayBottles = 0
 totalPayout = 0
 counter = 1
@@ -13,17 +12,17 @@ keepGoing = 'y'
 while keepGoing == 'y':
 	  	# Step 3: Code to set value of variables
     totalBottles = 0
+
       	# code to set value of variable totalBottles
     for counter in range(1, 8):
         todayBottles = int(input("Enter number of bottles returned for day #" + str(counter) + ': '))
         totalBottles = totalBottles + todayBottles
         counter = counter + 1
-    else:
-        break
+
       	# code to set value of variable totalPayout
-PayoutPerBottle = 0.10
-totalPayout = totalBottles * PayoutPerBottle
+    PayoutPerBottle = 0.10
+    totalPayout = totalBottles * PayoutPerBottle
 		# code to print the number of total bottles and total payout
-print('The total number of bottles collected is: ', totalBottles)
-print(f'The total paid out is ${totalPayout: .2f}')	
-keepGoing = input("Do you want to enter another week's worth of data? (Enter y or n): ")
+    print('The total number of bottles collected is: ', totalBottles)
+    print(f'The total paid out is ${totalPayout: .2f}')	
+    keepGoing = input("Do you want to enter another week's worth of data? (Enter y or n): ")
